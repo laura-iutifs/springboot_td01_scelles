28fd3503225944ba48c09ab51525ba044e81e910
laura-iutifs <laurascelles18@gmail.com>
2019-02-01 14:50:00 +0100

    first commit

---
+++ src/main/java/s4/spring/models/Element.java
@@ -0,0 +1,35 @@
+package s4.spring.models;
+
+public class Element {
+	private String nom;
+	private int evaluation;
+
+	// -----------------------------------
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	// -----------------------------------
+	public int getEvaluation() {
+		return evaluation;
+	}
+
+	public void setEvaluation(int evaluation) {
+		this.evaluation = evaluation;
+	}
+	// -----------------------------------
+
+	public boolean equalsItems(Object obj) {
+		if ((obj == null) || (obj.getClass() != this.getClass()) || nom == null)
+			return false;
+
+		if (nom == obj.getClass().getName())
+			return true;
+		
+		return false;
+	}
+}
---
+++ src/main/java/s4/spring/td0/Td01Application.java
@@ -0,0 +1,14 @@
+package s4.spring.td0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td01Application.class, args);
+	}
+
+}
+
